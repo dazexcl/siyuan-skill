68b6e82c03849b81c2a661c5874cfd351145b8c2
daze <dazehk@163.com>
2026-03-28 11:16:44 +0100

    chore: 更新版本号至1.7.5
    更新package.json、index.js和README.md中的版本号，保持文件间版本一致

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ class SiyuanNotesSkill {
   getInfo() {
     return {
       name: 'siyuan-skill',
-      version: '1.7.4',
+      version: '1.7.5',
       description: 'Siyuan Notes 连接器技能（支持向量搜索和 NLP）',
       commands: Object.keys(commands).map(cmd => ({
         name: cmd,
